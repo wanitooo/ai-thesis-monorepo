6b7e4f15b7c783a52abcc37ee957aebfa8e05e4e
wanito <juansantos2kplus2@gmail.com>
2023-11-02 14:59:50 +0800

    added call to upload-file and drnn-separate endpoint

--- src/pages/ToolPage.vue
+++ src/pages/ToolPage.vue
@@ -15,7 +15,7 @@
           v-model="rnn_media"
           label="Upload File"
           :dense="dense"
-          accept=".mp3"
+          accept=".wav"
         >
           <template v-slot:prepend>
             <q-icon name="upload" @click.stop.prevent />
@@ -71,6 +71,7 @@
             no-caps
             square
             label="Separate"
+            @click="handleDRNNSeparate"
             :disabled="!rnn_togglebtn"
           />
         </div>
@@ -178,6 +179,7 @@
             no-caps
             square
             label="Separate"
+            @click="handler"
             :disabled="!dprnn_togglebtn"
           />
         </div>
@@ -222,7 +224,7 @@
 import { ref, watch } from "vue";
 import { QMediaPlayer } from "@quasar/quasar-ui-qmediaplayer";
 import "@quasar/quasar-ui-qmediaplayer/src/index.sass";
-
+// import * as fs from "node:fs";
 export default {
   components: {
     QMediaPlayer,
@@ -235,24 +237,41 @@ export default {
     const dprnn_media = ref(null);
     const dprnn_mediaplayer = ref(null);
     const dprnn_togglebtn = ref(false);
-
+    const mixed_audio_path = ref(null);
+    const speaker1_audio_path = ref(null);
+    const speaker2_audio_path = ref(null);
     const itemUrl = ref(null);
 
     //This is just for testing
     const sources = [
       {
-        src: "",
-        type: "audio/mp3",
+        src: "backend/media/separated/drnn/spk1/1995-1837-0027_4970-29095-0022",
+        type: "audio/wav",
       },
     ];
 
     watch(
       () => rnn_media.value,
-      (file) => {
+      async (file) => {
         if (file && file.length > 0) {
-          console.log(file);
+          console.log("file ", " TYPE: ", typeof file, file);
           rnn_togglebtn.value = true;
-          loadFileBlob(file, 0);
+          const formData = new FormData();
+          formData.append("file", file[0]);
+          try {
+            const res = await fetch("http://127.0.0.1:8000/upload-file/", {
+              method: "POST",
+              // headers: {
+              //   'Content-Type': "multipart/form-data"
+              // },
+              body: formData,
+            }).then((res) => res.json());
+            console.log("fetch triggered, response ", res);
+            mixed_audio_path.value = res.file;
+            loadFileBlob(file, 0);
+          } catch (error) {
+            console.log("Something went wrong ", error);
+          }
         } else {
           rnn_togglebtn.value = false;
           loadFileBlob(null, 0);
@@ -280,17 +299,40 @@ export default {
         dprnn_mediaplayer.value.loadFileBlob(file);
       }
     }
+    async function handleDRNNSeparate() {
+      console.log(
+        "mixed_audio_path ",
+        JSON.stringify({ file: mixed_audio_path.value })
+      );
+      const res = await fetch("http://127.0.0.1:8000/drnn-separate/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ file: mixed_audio_path.value }),
+      }).then((res) => res.json());
+      console.log("Separate triggered, res ", res);
+      speaker1_audio_path.value = res.spk_1;
+      speaker2_audio_path.value = res.spk_2;
+    }
+    async function handler() {
+      console.log("Triggered handler");
+      // var output = fs.readFileSync("../../backend/requirements.txt");
 
+      console.log(output);
+    }
     return {
       //for RNN
       rnn_media,
       rnn_mediaplayer,
       rnn_togglebtn,
+      handleDRNNSeparate,
 
       //for DPRNN
       dprnn_media,
       dprnn_mediaplayer,
       dprnn_togglebtn,
+      handler,
 
       itemUrl,
       sources,
